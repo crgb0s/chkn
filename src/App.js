cdb26e79f302fd89bfd58e93875b12afa7ff0c6b
crgb0s <craigboswell@Craigs-MacBook-Pro.local>
2022-01-09 17:02:13 -0500

    addied chkn route

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path='' exact element={<Home />} />
+          <Route path='/chkn' element={<Home />} />
           <Route path='/' exact element={<Home />} />
           <Route path='/services' element={<Services />} />
           <Route path='/testimonials' element={<Testimonials />} />
